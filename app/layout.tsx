31d0ad62d53a66ef5993c05ce5d9a660e2588a00
zig wangles <153235983+zigwangles@users.noreply.github.com>
2025-03-27 00:30:43 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Tokyo Downloader',
-  description: 'Download videos from the web',
+  title: 'Tokydl',
+  description: 'Download audio from Tokybook',
 }
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
   )
